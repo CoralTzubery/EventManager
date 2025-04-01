9578f08f7bc7e04e6f563513a5669b05ea69b7d6
Coral Tzubery <tzubery.coral@gmail.com>
2025-04-01 19:50:15 +0300

    adding a post option for adding events

--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -1,11 +1,13 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { events, findEventById } from '../data/events.data';
+import { Event } from '../models/event.model';
 
 export const handleEventRoute = (req: IncomingMessage, res: ServerResponse) => {
     if (req.method === 'GET' && req.url === '/events') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(events));
     } 
+
     else if (req.method === 'GET' && req.url?.startsWith('/events/')) {
         const id = parseInt(req.url.split('/')[2]);
         const event = findEventById(id);
@@ -18,6 +20,28 @@ export const handleEventRoute = (req: IncomingMessage, res: ServerResponse) => {
             res.end(JSON.stringify(event));
         }
     }
+    
+    else if (req.method === 'POST' && req.url === '/events') {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            try {
+                const newEvent: Event = JSON.parse(body);
+                newEvent. id = events.length ? events[events.length - 1].id + 1 : 1;
+                events.push(newEvent);
+                res.writeHead(201, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(newEvent));
+            } catch (error) {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify( { message: 'Not a right JSON format'}));
+            }
+        });
+    }
+
     else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Event was not found'}));
