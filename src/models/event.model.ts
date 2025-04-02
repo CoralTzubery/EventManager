fe75fcf8eb7f0b2b4e7827ea78e76c35dbe0fae7
Coral Tzubery <tzubery.coral@gmail.com>
2025-04-02 07:43:11 +0300

    creating a schema for the events

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -1,3 +1,5 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
 export interface Event {
     id: number;
     name: string;
@@ -5,3 +7,19 @@ export interface Event {
     location: string;
     description: string;
 }
+
+export interface IEvent extends Document {
+    name: string;
+    date: Date;
+    loaction: string;
+    description: string;
+}
+
+const EventSchema = new Schema<IEvent>({
+    name: { type: String, required: true },
+    date: { type: Date, required: true },
+    loaction: { type: String, required: true },
+    description: { type: String, required: true }
+});
+
+export const EventModel = mongoose.model<IEvent>('Event', EventSchema);
